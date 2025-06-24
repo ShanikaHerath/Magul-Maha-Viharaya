6cb5e31c6c4a434d13a9011d05ba448fba6faeed
ShanikaHerath <shanikaherath248@gmail.com>
2025-06-24 19:07:22 +0530

    Add responsive navbar with hamburger menu

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
+import Home from './components/pages/Home/Home';
 import Footer from './components/Footer/Footer';
 import About from './components/pages/About/About';
 import Contact from './components/pages/Contact/Contact';
 import Donations from './components/pages/Donations/Donations';
 import Events from './components/pages/Events/Events';
 import Gallery from './components/pages/Gallery/Gallery'; // fixed typo: 'Galley' to 'Gallery'
-import Home from './components/pages/Home/Home';
 import Team from './components/pages/Team/Team';
-import ScrollToTop from './components/ScrollToTop'; // <-- import here
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/Magul-Maha-Viharaya"> {/* THIS FIXES HOMEPAGE LOADING ON GITHUB PAGES */}
       <div className="App">
         <Navbar />
-        <ScrollToTop /> {/* <-- add ScrollToTop here */}
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
@@ -31,5 +31,6 @@ function App() {
     </Router>
   );
 }
+
 
 export default App;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,21 +1,33 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "./Navbar.css";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => setIsOpen(!isOpen);
+  const closeMenu = () => setIsOpen(false);
+
   return (
     <nav className="navbar">
       <div className="navbar-logo">
+        <Link to="/">Magul Maha Viharaya</Link>
       </div>
-      <ul className="navbar-links">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/events">Events</Link></li>
-        <li><Link to="/gallery">Gallery</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
-        <li><Link to="/donations">Donate</Link></li>
-        
-    
+
+      <div className="hamburger" onClick={toggleMenu}>
+        <span className={`bar ${isOpen ? 'open' : ''}`}></span>
+        <span className={`bar ${isOpen ? 'open' : ''}`}></span>
+        <span className={`bar ${isOpen ? 'open' : ''}`}></span>
+      </div>
+
+      <ul className={`navbar-links ${isOpen ? 'open' : ''}`}>
+        <li><Link to="/" onClick={closeMenu}>Home</Link></li>
+        <li><Link to="/about" onClick={closeMenu}>About</Link></li>
+        <li><Link to="/donations" onClick={closeMenu}>Donations</Link></li>
+        <li><Link to="/events" onClick={closeMenu}>Events</Link></li>
+        <li><Link to="/gallery" onClick={closeMenu}>Gallery</Link></li>
+        <li><Link to="/team" onClick={closeMenu}>Team</Link></li>
+        <li><Link to="/contact" onClick={closeMenu}>Contact</Link></li>
       </ul>
     </nav>
   );
